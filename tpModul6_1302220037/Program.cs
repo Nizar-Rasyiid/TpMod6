e47b625ede7c4eff5c873742ec695e2e6ad8d8ff
NIZAR RASYIID <nizarrasyiid1221@gmail.com>
2024-03-25 01:13:56 +0700

    menambahkan method design contract

--- tpModul6_1302220037/Program.cs
+++ tpModul6_1302220037/Program.cs
@@ -6,12 +6,12 @@ public class SayaTubeVideo
     private string title;
     private int playCount;
 
-    public int getPlayCount(int playCount)
-    {
-        return playCount;
-    }
     public SayaTubeVideo(string title)
     {
+        if (string.IsNullOrEmpty(title) || title.Length > 100 )
+        {
+            throw new ArgumentException("Title Tidak Boleh NULL dan Lebih dari 100 karakter");
+        }
         Random random = new Random();
         id = random.Next(10000, 100000);
         this.title = title;
@@ -21,7 +21,21 @@ public class SayaTubeVideo
 
     public void IncreasePlayCount(int playCount)
     {
-        this.playCount += playCount;
+        if (playCount > 10000000)
+        {
+            throw new ArgumentOutOfRangeException("Pemanggilan Tidak Boleh Di atas 10.000.000");
+        }
+        try
+        {
+            checked
+            {
+                this.playCount += playCount;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
     }
 
     public void PrintVideoDetails()
@@ -37,8 +51,53 @@ class Program
 {
     public static void Main(string[] args)
     {
-        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - NIZAR RASYIID");
-        video.IncreasePlayCount(2000);
-        video.PrintVideoDetails();
+
+        //Test Title
+        try
+        {
+            SayaTubeVideo test1 = new SayaTubeVideo("In today's world, staying organized is crucial for productivity. Prioritize tasks, set goals, and use tools for efficiency. Mindfulness and breaks maintain focus. Balance work and life for well-being. Embrace strategies for confidences");
+            test1.IncreasePlayCount(1);
+            test1.PrintVideoDetails();
+        }
+        catch(Exception e) 
+        {
+            Console.WriteLine(e.Message);
+        }
+        //Test null
+        try
+        {
+            SayaTubeVideo test2 = new SayaTubeVideo("");
+            test2.IncreasePlayCount(1);
+            test2.PrintVideoDetails();
+        }catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        //test overflow
+        try
+        {
+            SayaTubeVideo tes3 = new SayaTubeVideo("Yang ini Tes Overflow");
+            tes3.IncreasePlayCount(1000000000);
+            tes3.PrintVideoDetails();
+        }catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+
+        //Refer to Num3
+        try
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - NIZAR RASYIID");
+            video.IncreasePlayCount(2000);
+            video.PrintVideoDetails();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+
     }
 }
